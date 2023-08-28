20d51d14e5ee43d1489dfe403cba0a03fbd36960
binaminhussein-100 <binamin.h.hassan14@gmail.com>
2023-08-28 14:42:30 +0300

    update: Pull main branch to update

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Join = () => {
+  const [email,setEmail] = useState('')
+
+  let theWaitlist = {
+      email:email
+  }
+
+  let submitEmail = (e) =>{
+    e.preventDefault()
+    
+    if (email.trim() !== '') {
+      fetch('http://localhost:8080/Emails', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(theWaitlist)
+      })
+        .then(() => {
+          console.log('Successfully submitted');
+        })
+        .catch((error) => {
+          console.error('Error submitting:', error);
+        });
+    }
+  }
+
   return (
-    <form>
-        <input type='email' pattern='' placeholder='Enter your email address' />
+    <form onSubmit={submitEmail}>
+        <input type='email'  pattern='[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}' placeholder='Enter your email address' value={email} onChange={(e)=>setEmail(e.target.value)} />
         <button>Join the Waitlist</button>
     </form>
 

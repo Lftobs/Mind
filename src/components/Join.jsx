44eff624de1ce5b434c1d175425137874d5690ae
tobsdev12 <tobbytobs1@gmail.com>
2023-08-31 18:53:30 +0100

    more changes

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -11,7 +11,7 @@ export const Join = () => {
 
   const handleClick = () => {
     setClick(true)
-    setMsg('Yaay! you have been added to the wait-list.')
+    setMsg('Yaay! you have been added to the waitlist.')
     setTimeout( () => {setClick(false), setMsg('Join the waitlist')}, 6000)
   }
 
